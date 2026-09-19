e894092a95c00b83ba1e3af3696bec79d265e3b8
Alonso <161665596+Alonso-Santos@users.noreply.github.com>
2026-09-18 22:05:29 -0300

    continuação aula 08

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 class Aluno:
     def __init__(self, note1, note2, note3, note4):
@@ -21,3 +22,40 @@ class Aluno:
             return"Reprovado"
     def generate_notes_lists(self):
         return [self.note1, self.note2, self.note3, self.note4]
+        
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(
+    BASE_DIR,"manager_notes", "manager_notes", "templates"
+)
+STATIC_DIR = os.path.join(
+    BASE_DIR, "manager_notes", "manager_notes", "static" 
+)
+
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = nome # Enquanto não houver envio de formulário, não há resultado
+    if request.method == "POST":
+
+        nome = request.form.get("nome")
+        note1 = request.form.get("note1")
+        note2 = request.form.get("note2")
+        note3 = request.form.get("note3")
+        note4 = request.form.get("note4")
+
+        aluno = Aluno(nome, note1, note2, note3, note4)
+
+        media = aluno.calculate_mean()
+        situacao = aluno.obter_situacao()
+
+        result = {
+            "nome":aluno.nome,
+            "notas":aluno.generate_notes_lists(),
+            "media": media,
+            "situacao": situacao,
+        }
+
+        return render_template("index.html", result=result)
+    if __name__ == "__main__":
+        app.run(debug=True)
